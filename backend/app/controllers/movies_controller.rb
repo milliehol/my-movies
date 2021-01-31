392f477c1336e689b01ae92a119cbf0db2ce6978
Carla <carla.holcomb25@gmail.com>
2021-01-31 12:27:37 -0500

    updated movie controller

--- backend/app/controllers/movies_controller.rb
+++ backend/app/controllers/movies_controller.rb
@@ -1,2 +1,36 @@
 class MoviesController < ApplicationController
+  def index
+   @movies = Movie.all
+   render json: @movies
+ end
+
+ def create
+   @movie = Movie.create(movie_params)
+   render json: @movie
+ end
+
+ def destroy
+   @movie = Movie.find_by_id(params[:id])
+   if @movie.destroy
+     render json: @movie
+   else
+     render json: {errors: @movie.errors.full_messages}, status: 422
+   end
+ end
+
+ def update
+   @movie = Movie.find_by_id(params[:id])
+
+   @movie.update(movie_params)
+   if @movie.save
+     render json: @movie
+   else
+     render json: {errors: @movie.errors.full_messages}, status: 422
+   end
+ end
+
+ private
+ def movie_params
+   params.permit(:title, :genre_id, genre_attributes: [:name])
+ end
 end
