f4966b37d4e9baa0b7d4cc7c5e8ca0137d594143
Carla <carla.holcomb25@gmail.com>
2021-01-31 12:28:56 -0500

    updated routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :movies
-  resources :genres
+  resources :movies, only: [:index, :update, :destroy]
+  resources :genres, only: [:index, :update, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
