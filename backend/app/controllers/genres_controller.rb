13b57529841baff26a04fb0c0918782afd20adc8
Carla <carla.holcomb25@gmail.com>
2021-01-31 12:25:04 -0500

    updated genre controller

--- backend/app/controllers/genres_controller.rb
+++ backend/app/controllers/genres_controller.rb
@@ -1,2 +1,28 @@
 class GenresController < ApplicationController
+   def index
+   @genres = Genre.all
+   render json: @genres
+ end
+
+ def create
+   @genre = Genre.create(genre_params)
+   render json: @genre
+ end
+
+ def update
+   @genre = Genre.find_by_id(params[:id])
+
+   if @genre.update(genre_params)
+     render json: @genre
+   else
+     render json: {errors: @genre.errors.full_messages}, status: 422
+   end
+ end
+
+
+ private
+
+ def genre_params
+   params.permit(:name)
+ end
 end
