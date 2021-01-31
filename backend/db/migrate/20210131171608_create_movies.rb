ff0475565bb3f8c979a73981e7fdca6dff6d9ff7
Carla <carla.holcomb25@gmail.com>
2021-01-31 12:30:16 -0500

    updated migrate file for movies

--- backend/db/migrate/20210131171608_create_movies.rb
+++ backend/db/migrate/20210131171608_create_movies.rb
@@ -1,7 +1,8 @@
 class CreateMovies < ActiveRecord::Migration[6.1]
   def change
     create_table :movies do |t|
-      t.string :name
+      t.string :title
+      t.belongs_to :genre, foreign_key: true
 
       t.timestamps
     end
