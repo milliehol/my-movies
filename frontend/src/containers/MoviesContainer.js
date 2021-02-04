9525eab6b7bbe3231525d580f0e0dcda3083e5b6
Carla <carla.holcomb25@gmail.com>
2021-02-04 12:28:06 -0500

    updated movies container

--- frontend/src/containers/MoviesContainer.js
+++ frontend/src/containers/MoviesContainer.js
@@ -1,33 +1,27 @@
 import React, { Component } from 'react';
 import MovieInput from '../components/movies/MovieInput'
 import Movies from '../components/movies/Movies'
-import { connect } from 'react-redux'
+
 
 class MoviesContainer extends Component {
   render() {
     return (
       <div>
-        <MovieInput
-          addMovie={this.props.addMovie}
-          genreId={this.props.genre.id}
-        />
-        <Movies
-          movies={this.props.movies}
-          genreId={this.props.genre.id}
-          deleteMovie={this.props.deleteMovie}
-        />
+          <MovieInput account={this.props.movie}/><br/>
+          <Movies transactions={this.props.genre && this.props.genre.movies}/>
       </div>
+
     );
   }
 }
 
-const mapStateToProps = ({movies}) => {
-  return {movies}
-}
+//const mapStateToProps = ({movies}) => {
+//  return {movies}
+//}
 
-const mapDispatchToProps = dispatch => ({
-  addMovie: movie => dispatch({type: 'ADD_MOVIE', title}),
-  deleteMovie: id => dispatch({type: 'DELETE_MOVIE', id})
-})
+//const mapDispatchToProps = dispatch => ({
+//  addMovie: movie => dispatch({type: 'ADD_MOVIE', title}),
+//  deleteMovie: id => dispatch({type: 'DELETE_MOVIE', id})
+//})
 
-export default connect(mapStateToProps, mapDispatchToProps)(MoviesContainer)
+export default MoviesContainer
