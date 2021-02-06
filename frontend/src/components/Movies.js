9ea6525849fcf517dfa334a4495e2fdeb9455829
Carla <carla.holcomb25@gmail.com>
2021-02-06 15:03:10 -0500

    updated movies.js

--- frontend/src/components/Movies.js
+++ frontend/src/components/Movies.js
@@ -1,46 +1,30 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {fetchMovies} from '../actions/fetchMovies'
+import {deleteMovie} from '../actions/deleteMovie'
 import Movie from './Movie';
 
 class Movies extends React.Component {
 
 
 
-//  componentDidMount() {
-//this.props.fetchMovies(genreId)
-//}
+
+  state = {}
+
+   handleDelete = (movie) => {
+      this.props.deleteMovie(movie.id, movie.genre_id)
+    }
+
+
 
   render() {
+    return (
+        <div>
+          {this.props.movies && this.props.movies.map(movie =>
+            <li key={movie.id}>{transaction.title} <button onClick={() => this.handleDelete(movie)}>Delete</button></li>
+          )}
+        </div>
+      )
+    }
+  }
 
-
-
-     const { movies, genreId, deleteMovie } = this.props;
-     this.props.fetchMovies({genreId: this.props.genreId })
-     console.log(genreId)
-     console.log(movies)
-     const associatedMovies = movies.filter(movie => movie.genre_Id === genreId);
-
-     const movieList = associatedMovies.map((movie, index) => {
-       return <Movie key={index} movie={movie} deleteMovie={deleteMovie} />
-     })
-
-     return (
-       <div>
-         <ul>
-           {movieList}
-         </ul>
-       </div>
-     );
-   }
-
- };
-
- const mapStateToProps = state => {
-   return {
-     movies: state.movies
-   }
- }
-
-
-export default connect(mapStateToProps, {fetchMovies})(Movies)
+  export default connect(null, {deleteMovie})(Movies)
